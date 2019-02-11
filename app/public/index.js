0cb94a88946b0bc28ae426471cdcea388585cc6a
The third meow <jsheaandcats@gmail.com>
2019-02-11 21:37:20 +1300

    allow front-end to access back-end data

--- app/public/index.js
+++ app/public/index.js
@@ -1,11 +1,30 @@
 
 /* eslint-disable no-new */
 /* eslint-disable no-undef */
+/* eslint-disable no-console */
 new Vue({
   el: '#app',
   data: {
-    stocks: {
-      BTC: 1302,
+    BTCprice: 'BTC price not yet loaded',
+  },
+  mounted() {
+    this.FetchBTCPrice();
+  },
+  methods: {
+    FetchBTCPrice() {
+      this.$http.get('/api/stockprice/BTC')
+        .then(response => response.json())
+        .then((result) => {
+          if (result.price === '') {
+            this.BTCprice = 'BTC price is unknown';
+          } else {
+            this.BTCprice = result.price;
+          }
+        }).catch((err) => {
+          console.log(err);
+        });
+      // return 'BTC price is unavailable';
     },
   },
+
 });
--- app/server.js
+++ app/server.js
@@ -8,7 +8,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  // ?res.sendHeader({'content-type':'html'});
   res.sendFile('index.html');
+});
+
+app.get('/api/stockprice/BTC', (req, res) => {
+  res.send({ price: '1302' });
 });
 
 app.listen(port, () => console.log(`Profile Nine is running. Vist localhost:${port}`));
