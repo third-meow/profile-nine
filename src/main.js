d48a71c02a49e3622de401b3dd557a122030e968
The third meow <jsheaandcats@gmail.com>
2019-02-22 18:33:18 +1300

    add 'clunky' routing

--- src/main.js
+++ src/main.js
@@ -2,13 +2,41 @@
 /* eslint-disable no-undef */
 import Vue from 'vue/dist/vue';
 import VueResource from 'vue-resource';
-import App from './App.vue';
+
+import Index from './pages/Index.vue';
+import Price from './pages/Price.vue';
+import PageNotFound from './pages/PageNotFound.vue';
 
 Vue.use(VueResource);
 Vue.config.devtools = true;
 
+
+const routes = {
+  data: {
+    '/': Index,
+    '/price': Price,
+  },
+
+  bestFit(path) {
+    if (this.data[path] !== undefined) {
+      return this.data[path];
+    }
+    if (this.data[`/${path.split('/')[1]}`] !== undefined) {
+      return this.data[`/${path.split('/')[1]}`];
+    }
+    return PageNotFound;
+  },
+};
+
+
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App },
+  computed: {
+    ViewComponent() {
+      return routes.bestFit(window.location.pathname);
+    },
+  },
+  render(h) {
+    return h(this.ViewComponent);
+  },
 });
